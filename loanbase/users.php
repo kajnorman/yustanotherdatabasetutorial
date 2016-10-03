612593a6e11cc00cc8e0e4821c2e9316c063b5a1
kajnorman <knn@eaaa.dk>
2016-10-03 22:20:17 +0200

    Update users.php

--- loanbase/users.php
+++ loanbase/users.php
@@ -3,17 +3,6 @@
 <?php
 include_once 'database_connection.php';
 
-$sql1 = "select * from borrowers;";
-$result = mysqli_query($conn, $sql1);
-
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo  $row["Borrower_Name"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
 
 ?>
 
@@ -27,18 +16,30 @@ New User name:<br>
 <?php
 $username = $_GET["username"]; // Remember to sanitize!!
 if  (!empty($_GET["username"])) {
-	echo "username is set <br>";
-	$sql2 = "INSERT INTO borrowers (Borrower_Name) VALUES ('$username')";
-	if (mysqli_query($conn, $sql2)) {
-	    echo "New record created successfully";
-	} else {
-    		echo "Error: " . $sql2 . "<br>" . mysqli_error($conn);
-		}
-	}
+        echo "username is set <br>";
+        $sql2 = "INSERT INTO borrowers (Borrower_Name) VALUES ('$username')";
+        if (mysqli_query($conn, $sql2)) {
+            echo "New record created successfully<br>";
+        } else {
+                echo "Error: " . $sql2 . "<br>" . mysqli_error($conn);
+                }
+        }
 else { echo "username is not set "; }
 
+$sql1 = "select * from borrowers;";
+$result = mysqli_query($conn, $sql1);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo  $row["Borrower_Name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
 
 
 mysqli_close($conn);
 echo "<br>";
 include_once 'footer.php';
+
